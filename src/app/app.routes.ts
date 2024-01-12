f8c2b78e04c32dffd0a16096e6f51710e3bf1dc3
Marvin Senkiv <127746129+marvinsenkiv@users.noreply.github.com>
2024-01-12 17:16:28 -0300

    componente navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
+import { ContatoComponent } from './pages/contato/contato.component';
+import { ServicosComponent } from './pages/servicos/servicos.component';
 import { Routes } from '@angular/router';
+import { InicioComponent } from './pages/inicio/inicio.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: InicioComponent },
+    { path: 'servicos', component: ServicosComponent },
+    { path: 'contato', component: ContatoComponent },
+];
