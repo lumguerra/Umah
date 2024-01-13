62c6cae5b0fc1c37e182d31b068e1ca2a1a1f138
Marvin Senkiv <127746129+marvinsenkiv@users.noreply.github.com>
2024-01-12 23:16:56 -0300

    inicio serviços

--- src/app/components/inicio-servicos/inicio-servicos.component.ts
+++ src/app/components/inicio-servicos/inicio-servicos.component.ts
@@ -1,12 +1,29 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-inicio-servicos',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './inicio-servicos.component.html',
   styleUrl: './inicio-servicos.component.css'
 })
 export class InicioServicosComponent {
-
+  services = [
+    {
+      title: 'Serviço 1',
+      text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been.',
+      image: '../../../assets/sethscope.png'
+    },
+    {
+      title: 'Serviço 2',
+      text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been.',
+      image: '../../../assets/brain.png'
+    },
+    {
+      title: 'Serviço 3',
+      text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been.',
+      image: '../../../assets/card.png'
+    },
+  ];
 }
